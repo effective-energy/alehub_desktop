4c716be7b6af226558035876e025d429eb03c227
Denis <denisqwe41@gmail.com>
2018-06-13 12:35:13 +0300

    Update run script for linux

--- resources/linux/run.sh
+++ resources/linux/run.sh
@@ -1,18 +1,18 @@
 #! /bin/bash
 
-#set(USE_SNAPPY_DEFAULT 0)
 mkdir $HOME/.local
 mkdir $HOME/.local/share
 mkdir $HOME/.local/share/ale
+mkdir $HOME/.local/share/ale/linux
 rm $HOME/.local/share/ale/genesis.hash
 rm $HOME/.local/share/ale/genesis.json
+rm -r $HOME/.local/share/ale/linux/*
 cp genesis.hash $HOME/.local/share/ale/
 cp genesis.json $HOME/.local/share/ale/
-#echo "7c18c65170e1198313afe120ef070210c374ef733b3008e336d37698e5517b64"  >> /Users/ss/.local/share/ale/genesis.hash
-#export LD_LIBRARY_PATH=./lib
+cp -av ./* $HOME/.local/share/ale/linux
+cd $HOME/.local/share/ale/linux
 export DYLD_LIBRARY_PATH="./lib"
 export LD_LIBRARY_PATH=./lib
-#otool -L lib/libsnappy.a
 export LDFLAGS='-L/lib'
 #export WITH_SNAPPY=/lib
 #export SNAPPY=/lib
